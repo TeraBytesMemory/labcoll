95251c89cba013053c814f574e63f4e447ecda84
TeraBytesMemory <yuj.terazawa@gmail.com>
2016-12-06 14:43:27 +0900

    [Add] add pagenation in research_controller

--- app/controllers/research_controller.rb
+++ app/controllers/research_controller.rb
@@ -1,9 +1,10 @@
 class ResearchController < ApplicationController
   def index
-    @projects = Project.page(1)
+    @projects = Project.page(1).per(9)
   end
 
   def list
     @research = Research.find(params[:id])
+    @projects = Kaminari.paginate_array(@research.projects).page(params[:page]).per(9)
   end
 end
