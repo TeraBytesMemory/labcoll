b386d6477f863926843e08d4fa332db86b5d54d6
TeraBytesMemory <yuj.terazawa@gmail.com>
2016-12-06 15:32:34 +0900

    [Add] add order of projects at Research model

--- app/models/research.rb
+++ app/models/research.rb
@@ -2,7 +2,7 @@ class Research < ActiveRecord::Base
   has_many :laboratories
 
   has_many :researches_projects
-  has_many :projects, :through => :researches_projects
+  has_many :projects, ->{order('id')}, :through => :researches_projects
 
   belongs_to :research_branch
 end
