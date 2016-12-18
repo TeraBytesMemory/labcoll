8a09d1ee1bf1dffdaf74975614f75ece86ad9f5c
TeraBytesMemory <yuj.terazawa@gmail.com>
2016-12-18 16:30:19 +0900

    [Fix] fix some ui

--- app/models/research.rb
+++ app/models/research.rb
@@ -5,4 +5,9 @@ class Research < ActiveRecord::Base
   has_many :projects, ->{order('id')}, :through => :researches_projects
 
   belongs_to :research_branch
+
+  def research_brothers
+    research_branch = ResearchBranch.find(self.research_branch)
+    research_branch.researches
+  end
 end
