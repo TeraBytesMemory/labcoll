f820733a6129b242f8cbf08653fb3d0387df0bc2
TeraBytesMemory <yuj.terazawa@gmail.com>
2016-12-24 15:42:08 +0900

    [Add] add page_title helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,4 +1,11 @@
 module ApplicationHelper
+
+  def page_title
+    title = "LabCollection"
+    title = @page_title + " | " + title if @page_title
+    title
+  end
+
   def load_sidebar_content
     ResearchBranch.all
   end
