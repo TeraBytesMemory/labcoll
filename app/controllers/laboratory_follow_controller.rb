b0b1f7af07a859586249ccfbec5f0e1bfe8f8d2e
TeraBytesMemory <yuj.terazawa@gmail.com>
2016-12-28 15:48:17 +0900

    [Add] add follow system

--- app/controllers/laboratory_follow_controller.rb
+++ app/controllers/laboratory_follow_controller.rb
@@ -1,22 +1,20 @@
 class LaboratoryFollowController < ApplicationController
-  before_action :logged_in_user
+  before_action :authenticate_user!
 
   def create
-    @user = current_user
     @laboratory = Laboratory.find(params[:following_id])
 
-    @user.laboratory_follow @laboratory
+    current_user.laboratory_follow @laboratory
     respond_to do |format|
       format.html { redirect_to @laboratory }
       format.js
     end
   end
 
-  def destory
-    @user = current_user
+  def destroy
     @laboratory = Laboratory.find(params[:following_id])
 
-    @user.unlaboratory_follow @laboratory
+    current_user.unlaboratory_follow @laboratory
     respond_to do |format|
       format.html { redirect_to @laboratory }
       format.js
--- config/routes.rb
+++ config/routes.rb
@@ -4,9 +4,12 @@ Rails.application.routes.draw do
   get '/', to: 'research#index'
   get '/research/:id', to: 'research#list'
 
-  get '/detail/:id', to: 'laboratory#detail'
+  get '/laboratory/:id', to: 'laboratory#detail'
 
   get '/user', to: 'user#home'
+
+  post '/laboratory/follow', to: "laboratory_follow#create"
+  delete '/laboratory/follow', to: "laboratory_follow#destroy"
 
   get '*path', controller: 'application', action: 'render_404'
 end
